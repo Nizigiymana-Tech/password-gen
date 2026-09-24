9c80b35e02c3238c1805f0509a1042af508b26dd
Nizigiymana-Tech <junkacc830@gmail.com>
2026-09-24 10:25:59 -0400

    Pushed code

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # Put any imports up here:
-
+import pyperclip
+import random
 
 '''
 Q1. Print out the 4th letter ("r") from the string name using bracket notation and the index.
 Syntax: variable_name[index]
 '''
 name = "snorlax"
-print("Q1. The 4th letter is:")
-
-
+print(f"Q1. The 4th letter is: {name[3]}")
 
 '''
 Q2. Strings are iterable, too! You can treat them like a list.
@@ -24,7 +23,8 @@ print("\nQ2. A random letter is:")
 Q3. Print out 5 random characters from letters using a for-loop.
 '''
 print("\nQ3. Five nights... I mean 5 random characters are:")
-
+for x in range(1, 6):
+    print(random.rand)
 
 
 
@@ -55,6 +55,10 @@ phone_book = {
     "WTHS": "508-799-1940"
 }
 
+name = input("enter someone's name: ")
+phone = input(f"Enter {name} phone number: ")
+
+phone_book[name] = phone
 
 '''
 Q8. Print out all the phone numbers with a for-loop in the format:
@@ -63,4 +67,5 @@ https://www.w3schools.com/python/python_dictionaries_loop.asp
 '''
 print("\nMy Phone Book:")
 
-
+for name, phone in phone_book.items():
+    print(f"{name}'s phone number: {phone}")
